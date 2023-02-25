07ff59bd5d6ad97d73be890a121f90bcf20bdf6e
sghosh79 <shu.ghosh@gmail.com>
2023-02-24 20:02:04 -0500

    eCommerce site with React

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
+
+export default class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavBar />
+        <div className="container-fluid">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/customers" element={<CustomersList />} />
+            <Route path="/cart" element={<ShoppingCart />} />
+            <Route path="*" element={<NoMatchPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
